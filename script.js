380412bd4715e95679dedb71a11916f7fc31cbc0
Douglasluis508 <douglasluis508@gmail.com>
2023-06-25 15:13:30 -0300

    aula guiada objetos

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-function imprimirObjeto () {
-    //digite a sua solução aqui
-}
-imprimirObjeto()
+
+    const estudante = {
+        nome: 'Douglas',
+        sobrenome: 'Oliveira',
+        numeroMatricula: 123456789,
+        notasDoSemetre: [10,8,7,9,7,8.5],
+        
+    }
+
+
+
+estudante.modulo = 1
+// console.log(estudante);
+
+// console.log(estudante.nome);
+// console.log(estudante.notasDoSemetre[1]);
+// console.log(estudante.modulo);
+
+const copiaEstudante = {
+  ...estudante,
+  nome:'Astrodev', 
+  notasDoSemetre: [estudante.notasDoSemetre,9],
+  modulo: estudante.modulo+1
+};
+
+//console.log(copiaEstudante);
+
+const estudantesLabenu = []
+copiaEstudante.push([estudantesLabenu])
+console.log(estudantesLabenu);
